d38a94145159d6bfc8ba706dd4f97ca61c6de326
Sara Cook <saracook@stanford.edu>
2024-03-20 09:59:07 -0700

    UX additions

---
+++ includes/js/templates.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#identityBar').load('/includes/html/identitybar.html');
+  console.log('footer');
+  $('footer').load('/includes/html/footer.html');
+$('#masthead').load('/includes/html/masthead.html');
+$('#skip-link').load('/includes/html/skiplink.html');
+});
